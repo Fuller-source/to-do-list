26c15fb6e04ebb9e824a96ee1fc6d072de16e44c
James Fuller <fuller.jdf@outlook.com>
2025-09-15 16:16:44 -0500

    Added 'completed' functionality

--- components/TodoItem.tsx
+++ components/TodoItem.tsx
@@ -1,19 +1,25 @@
 'use client'
 
-import {Todo} from "./TodoList";
+import {Todo} from "./TodoList"
 
 // When you are defining multiple props for a component, you create an interface to describe the shape of the props object, then I use that interface in the function signature
 interface TodoItemProps{
     todo: Todo;
+    onComplete: (id: number) =>void;
     onDelete: (id: number) => void;
+    
 }
 
-export const TodoItem = ({todo, onDelete}: TodoItemProps) => {
+export const TodoItem = ({todo, onDelete, onComplete}: TodoItemProps) => {
     
 
     return (
         <li className="bg-yellow-200 text-black p-2 rounded al">
-            {todo.task}
+            <input type="checkbox" checked={todo.completed} name="status"  value={todo.id} onClick={() => onComplete(todo.id)}/>
+            <span className={todo.completed ? 'line-through' : ''}>
+                {todo.task}
+            </span>
+            
             <button className="hover:bg-amber-600" onClick={() => onDelete(todo.id)}>-</button>
         </li>
     )
--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,14 +1,14 @@
 'use client'
 
-import React, {useState} from 'react';
-import { TodoForm } from './TodoForm';
-import { TodoItem } from './TodoItem';
-import { todo } from 'node:test';
+import React, {useState} from 'react'
+import { TodoForm } from './TodoForm'
+import { TodoItem } from './TodoItem'
+import { todo } from 'node:test'
 
 export interface Todo{
-    id: number;
-    task: string;
-    completed: boolean;
+    id: number
+    task: string
+    completed: boolean
 }
 
 export function List(){
@@ -28,12 +28,26 @@ export function List(){
 
     function handleDeleteTask(id: number){
         const newTodos = todos.filter(task => task.id != id)
-        setTodos(newTodos);
+        setTodos(newTodos)
     }
+
+    function handleCompleteTask(id: number){
+        const newTodos = todos.map(task => {
+            if (task.id === id){
+                if (task.completed == false){
+                    return {...task, completed: true} // ... copies all existing properties, and then I override the completed property with true, to ensure immutability.
+                }
+                return {...task, completed: false}
+            } 
+            return task
+        });
+        setTodos(newTodos)
+    }
+
     return(
             <ul>
                 {todos.map((todo) => (
-                    <TodoItem todo={todo} key={todo.id} onDelete={() => handleDeleteTask(todo.id)} />
+                    <TodoItem todo={todo} key={todo.id} onDelete={() => handleDeleteTask(todo.id)} onComplete={() => handleCompleteTask(todo.id)} />
                 ))}
                 <TodoForm onAdd={handleAddTask}/>
             </ul>
