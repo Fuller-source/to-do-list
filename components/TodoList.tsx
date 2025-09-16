379aaa1315278349ff45884515ff53f2315aeb9b
James Fuller <fuller.jdf@outlook.com>
2025-09-15 20:53:36 -0500

    Added data persistance in local storage

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,21 +1,39 @@
-'use client'
+'use client' // tells next that the componenet needs to run in browser, giving it access to browser APIs like local storage
 
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import { TodoForm } from './TodoForm'
 import { TodoItem } from './TodoItem'
 import { todo } from 'node:test'
 
-export interface Todo{
+export interface Todo{ // This is what makes ts valuable.
     id: number
     task: string
     completed: boolean
 }
 
 export function List(){
-    //const [todos, setToDos] = useState<ToDo[]> ([]);
-    const [todos, setTodos] = useState<Todo[]>([
-        
-    ]);
+    const [todos, setTodos] = useState<Todo[]>([]); // initializes the component's state. todos holds the list of tasks. setTodos is the function I use to update it.
+
+    /**
+     * This hook handles loading data.
+     * useEffect only runs the code once when the componenet first mounts, because its dependency array is empty.
+     * The code inside retrieves todos from localStorage, if they exist, it updates the state.
+     */
+    useEffect(() => {
+        const storedTodos = localStorage.getItem("todos")
+        if (storedTodos){
+            setTodos(JSON.parse(storedTodos))
+        }
+    }, [])
+
+    /**
+     * This hook handles saving data.
+     * This runs every time a variable in its dependency array changes [todos]
+     * The code converts the todos array to a string and saves it to localStorage, to ensure persistance.
+     */
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos));
+    },[todos])
 
     function handleAddTask(newTask: string){
         const todoItem = {
